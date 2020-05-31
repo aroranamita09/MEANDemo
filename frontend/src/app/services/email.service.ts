021b4bed0e55e0d84721cb3226de333b64dfeeab
Namita Arora <namita.arora@boeing.com>
2020-06-01 03:54:29 +0530

    added e-mail service

--- frontend/src/app/services/email.service.ts
+++ frontend/src/app/services/email.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  private emailURL ='http://ip/email';
+  private numberOfUsersAhead='http://ip/email';
+  constructor(private http: HttpClient) { }
+  // check validation
+  // and call post on click of submit from email-component
+  postUserEmail(emailId : string) : Observable <any>{
+    console.log('user email is  : ', emailId);
+    return this.http.post(this.emailURL, emailId, {responseType: 'text'});
+  }
+ // You're on the list with n others
+ getUsersAhead(){
+  return this.http.get(this.numberOfUsersAhead);
+ }
 }
